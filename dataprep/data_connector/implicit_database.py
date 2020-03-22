6dc12063841254f753adec80d89bc346905c2a37
yxie66 <zjuxyee@gmail.com>
2020-03-21 22:55:13 -0700

    reorganize code

--- dataprep/data_connector/implicit_database.py
+++ dataprep/data_connector/implicit_database.py
@@ -221,6 +221,7 @@ class ImplicitDatabase:
 
         self.name = path.name
         self.tables = {}
+        
 
         for table_config_path in path.iterdir():
             if not table_config_path.is_file():
@@ -232,7 +233,6 @@ class ImplicitDatabase:
             if table_config_path.suffix != ".json":
                 # ifnote non json file
                 continue
-
             with open(table_config_path) as f:
                 table_config = jload(f)
                 self.config = table_config
